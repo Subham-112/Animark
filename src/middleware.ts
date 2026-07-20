2f6547937610773da888c9345e88d68493605234
Subham-112 <subhambusiness286@gmail.com>
2026-07-20 16:30:15 +0530

    Updated middleware to redirect relavent path as per role access token

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,14 +2,18 @@ import { NextRequest, NextResponse } from "next/server";
 import { COOKIE_NAMES } from "./utils/cookies-names";
 
 export function middleware(req: NextRequest) {
-  const token = req.cookies.get(COOKIE_NAMES.USER_ACCESS_TOKEN)?.value;
+  const userAccess = req.cookies.get(COOKIE_NAMES.USER_ACCESS_TOKEN)?.value;
+  const sellerAccess = req.cookies.get(COOKIE_NAMES.SELLER_ACCESS_TOKEN)?.value;
+  const adminAccess = req.cookies.get(COOKIE_NAMES.SELLER_ACCESS_TOKEN)?.value;
   const pathname = req.nextUrl.pathname;
 
   if (pathname === "/") {
-    if (token) return NextResponse.redirect(new URL("/dashboard", req.url));
+    if (userAccess) return NextResponse.redirect(new URL("/dashboard", req.url));
+    if (sellerAccess) return NextResponse.redirect(new URL("/seller", req.url));
+    if (adminAccess) return NextResponse.redirect(new URL("/admin", req.url));
   }
 
-  if (pathname.startsWith("/dashboard") && !token) {
+  if (pathname.startsWith("/dashboard") && !userAccess) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
--- src/utils/cookies-names.ts
+++ src/utils/cookies-names.ts
@@ -1,4 +1,7 @@
 export const COOKIE_NAMES = {
   USER_ACCESS_TOKEN: "user_access",
   USER_REFRESH_TOKEN: "user_refresh",
+
+  SELLER_ACCESS_TOKEN: "seller_access",
+  SELLER_REFRESH_TOKEN: "seller_refresh"
 } as const;
